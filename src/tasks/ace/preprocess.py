45a7ef6cc2f01d802addb681eb69020de1b35096
ValentinaPy <valentina@posteo.de>
2023-04-20 15:45:21 -0700

    new exp

--- src/tasks/ace/preprocess.py
+++ src/tasks/ace/preprocess.py
@@ -25,6 +25,8 @@ def preprocess_ace(filepath: Union[str, Path]) -> pd.DataFrame:
         ques_str = row["query_question"]
         ans_str = row["argument_text"]
         ans_span = row["argument_span"]
+        ans_span = ans_span.split(':')
+        ans_span = [[int(ans_span[0]),int(ans_span[1])]]
         processed_data.append(
             [sent_id, sentence, predicate, ques_str, ans_str, ans_span])
 
